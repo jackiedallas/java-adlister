85e734fa5e2d513916cbe9c22a5106b037e67ef5
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-29 16:13:52 -0600

    finished exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,43 @@
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
 
+@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    int hitCount = 0;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException {
+        String searchQuery = req.getParameter("q");
+        String resetCount = req.getParameter("r");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String printHitCount = "<h3>" + hitCount + "</h3>";
+//        hitCount++;
+
+
+        if (searchQuery != null) {
+            out.printf("<h1>Hello, %s!</h1>", searchQuery);
+            hitCount++;
+        } else {
+            out.println("<h1>Hello, World!</h1>");
+            hitCount++;
+        }
+        out.println("<h2>Hit counter</h2>");
+        out.println(printHitCount);
+
+        if (resetCount.equalsIgnoreCase("true")) {
+            hitCount = 0;
+        }
+
+
+
+
+
 
     }
 }
