4a14711a58ff8a56a9a0e23c256f514c30630561
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-06 14:51:12 -0600

    created config and dao

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,8 @@
+public class MySQLAdsDao implements Ads {
+    private Connection connect;
+    private Config config = new Config();
+
+    public MySQLAdsDao() throws SQLException {
+
+    }
+}
---
+++ src/main/webapp/adlister_ads.sql
@@ -0,0 +1,8 @@
+USE adlister_db;
+
+DROP TABLE IF EXISTS ads;
+
+CREATE TABLE ads(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id
+)
